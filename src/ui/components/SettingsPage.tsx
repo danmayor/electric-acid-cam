3cb0bd2a97346a2102390cef6cef6eba29d44471
Dan Mayor <dmayor@digivance.com>
2022-10-26 19:44:38 -0400

    Componetizing page header

--- src/ui/components/SettingsPage.tsx
+++ src/ui/components/SettingsPage.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { MdOutlineSettingsApplications } from 'react-icons/md';
+import { Card, CardBody, CardHeader, Col, Input, Row } from 'reactstrap';
 import PageHeader from './common/PageHeader';
 
 const SettingsPage: React.FC = () => {
@@ -12,8 +13,20 @@ const SettingsPage: React.FC = () => {
             ]}
         />
 
-        <h2>Electric Acid Cam Launcher Settings</h2>
-        Coming soon
+        <Card>
+            <CardHeader>Application Settings</CardHeader>
+            <CardBody>
+                <Row>
+                    <Col xs={12} sm={6}>Acid Cam Path</Col>
+                    <Col xs={12} sm={6}><Input type="text" /></Col>
+                </Row>
+
+                <Row>
+                    <Col xs={12} sm={6}>Capture Path</Col>
+                    <Col xs={12} sm={6}><Input type="text" /></Col>
+                </Row>
+            </CardBody>
+        </Card>
     </>;
 };
 
